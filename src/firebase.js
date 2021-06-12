182caf8ceec21a79f71ffa37d6d9fd4de6b67725
Krishanu Chhabra <30947596+krishanuchhabra@users.noreply.github.com>
2021-06-13 02:37:34 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase"
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCJbwGdUt74LPxvNHl5xWmWAf2ArkzKzE0",
-  authDomain: "messengerapp-4cbaa.firebaseapp.com",
-  projectId: "messengerapp-4cbaa",
-  storageBucket: "messengerapp-4cbaa.appspot.com",
-  messagingSenderId: "544277255132",
-  appId: "1:544277255132:web:ac9ce44ffeacd3b31f3519",
-  measurementId: "G-NBDPQSGR4L"
+ //firebase config details here
 })
 
 const db = firebaseApp.firestore()
